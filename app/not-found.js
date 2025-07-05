9918304004c6f3dea1fd71f489acd025f8b9c258
Stephenpop <anyaibeebuka@gmail.com>
2025-07-05 13:06:48 +0100

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+// app/not-found.js
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export const viewport = {
+  colorScheme: 'light', // Match your desired color scheme
+};
